3aa9a143655aa12187974baad7281c1412686140
Alexander Sakhaev <alexander_sakhaev@mail.ru>
2019-03-10 04:46:44 +0300

    Update 1.js

--- dapplets/1.js
+++ dapplets/1.js
@@ -1,6 +1,9 @@
 ({
-  template: '<div>{{text}}</div>',
-  createTx: function(metadata) {
-    return {};
+  "template": "<div>{{id}}</div><div>{{text}}</div><div>{{authorFullname}}</div><div>{{authorUsername}}</div><img src=\"{{authorImg}}\"/>",
+  "to": "0xccf7930d9b1fa67d101e3de18de5416dc66bd852",
+  "function": "function storeTweetHash(uint tweetId, uint tweetHash)",
+  "mapping": {
+    "tweetId": "id",
+    "tweetHash": "sha256(text)"
   }
 })
